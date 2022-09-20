8e76f19aab1f18b59c5032a4fa282d1d12535f20
EC2 Default User <ec2-user@ip-172-31-19-77.ec2.internal>
2022-09-20 20:50:07 +0000

    [Finish] Strp2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -43,7 +43,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path, notice: "Book was successfully destroyed."
+    redirect_to books_path
   end
   
   
